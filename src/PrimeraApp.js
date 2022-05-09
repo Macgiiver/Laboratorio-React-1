5b9e14c90e88b4f27f919a5dc6db102a5c55887d
macgiiver <luis.reyest1995@gmail.com>
2022-05-09 15:12:20 -0500

    mensaje prueba con CSS

---
+++ src/PrimeraApp.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './index.css';
+
+const PrimeraApp = () => {
+    return (
+        <div>
+            <h1>Hola Mundo Gamer</h1>
+            <p>Aplicacion de laboratorio</p>
+        </div>
+    );
+}
+
+export default PrimeraApp;
